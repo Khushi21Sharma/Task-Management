a7918a81b44f735433230d2a25ecd3712bf7555e
Khushi21Sharma <140005227+Khushi21Sharma@users.noreply.github.com>
2024-06-24 18:41:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import * as ReactDOM from "react-dom/client";
-import { TaskProvider } from './context/TaskContext'; // Import the TaskProvider
-import { ToastContainer } from 'react-toastify';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthWrapper } from './context/AuthWrapper';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-
+// Create a root to render the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the application within React.StrictMode
 root.render(
   <React.StrictMode>
+    {/* Provide the context for tasks throughout the application */}
     <TaskProvider>
+      {/* Enable client-side routing using React Router */}
       <BrowserRouter>
+        {/* Wrap the application with authentication logic */}
         <AuthWrapper />
       </BrowserRouter> 
     </TaskProvider>
+    {/* Display toast notifications throughout the application */}
     <ToastContainer />
   </React.StrictMode>
 );
