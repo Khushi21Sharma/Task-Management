c37bb9d026bcd2bb6f3c170976318893e4fcc3d9
Khushi21Sharma <140005227+Khushi21Sharma@users.noreply.github.com>
2024-06-24 18:42:39 +0530

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,22 @@
+// Function to report web vitals performance metrics
 const reportWebVitals = onPerfEntry => {
+  // Check if onPerfEntry is provided and is a function
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // Dynamically import the 'web-vitals' library
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      // Collect and report the Cumulative Layout Shift (CLS) metric
       getCLS(onPerfEntry);
+      // Collect and report the First Input Delay (FID) metric
       getFID(onPerfEntry);
+      // Collect and report the First Contentful Paint (FCP) metric
       getFCP(onPerfEntry);
+      // Collect and report the Largest Contentful Paint (LCP) metric
       getLCP(onPerfEntry);
+      // Collect and report the Time to First Byte (TTFB) metric
       getTTFB(onPerfEntry);
     });
   }
 };
 
+// Export the reportWebVitals function as the default export of this module
 export default reportWebVitals;
