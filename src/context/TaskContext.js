98c36d57d989f240b6ed4d156f5060682167f281
Khushi21Sharma <140005227+Khushi21Sharma@users.noreply.github.com>
2024-06-24 19:03:43 +0530

    Update TaskContext.js

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -1,29 +1,37 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
+// Create a context for managing task-related state
 const TaskContext = createContext();
 
+// Custom hook to access task-related data and functions from the context
 export const useTaskContext = () => {
   return useContext(TaskContext);
 };
 
+// TaskProvider component responsible for managing task-related state
 export const TaskProvider = ({ children }) => {
+  // State variables for managing tasks, sorting order, status filter, and current page
   const [tasks, setTasks] = useState(() => {
+    // Initialize tasks from localStorage if available
     const storedTasks = localStorage.getItem('tasks');
     return storedTasks ? JSON.parse(storedTasks) : [];
   });
   const [sortOrder, setSortOrder] = useState('asc');
   const [filterStatus, setFilterStatus] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const tasksPerPage = 4;
+  const tasksPerPage = 4; // Number of tasks per page
 
+  // Effect to update localStorage whenever tasks state changes
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }, [tasks]);
 
+  // Function to add a new task
   const addTask = (newTask) => {
     setTasks([...tasks, newTask]);
   };
 
+  // Context value containing task-related data and functions
   const value = {
     tasks,
     setTasks,
@@ -37,5 +45,7 @@ export const TaskProvider = ({ children }) => {
     addTask
   };
 
+  // Render the TaskContext.Provider with the provided value
   return <TaskContext.Provider value={value}>{children}</TaskContext.Provider>;
 };
+
